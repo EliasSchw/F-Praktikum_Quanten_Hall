452b64a28b5be6e2cf0d98b010fbb838c5fb6982
Lukas <lukasxhein@gmail.com>
2025-04-09 16:59:24 +0200

    Bib fertig

--- Python/Preprocessing.py
+++ Python/Preprocessing.py
@@ -25,16 +25,21 @@ filepath_4_2K = r'RawData/1 0.dat'
 filepath_3K = r'RawData/18_23V2_970K_end_18_30V_2_970K 1.dat'
 filepath_2_1K = r'RawData/43_6V2_08K_end_44_8V_2_100K 3.dat'
 filepath_1_4K = r'RawData/133V1_520K_end_133_1V1_520K 4.dat'
-filepath_1_4K_switched = r'RawData/Gate_--1_49904V_LI_swiched_151_6V1_470K_end_133_3V1_520K 9.dat'
+filepath_1_4K_switched = r'RawData/LI_swiched_153_3V1_470K_end_135_1V1_520K 5.dat'
 filepath_Gate_minus_1_5V = r'RawData/Gate_--1_49904V_LI_swiched_151_6V1_470K_end_133_3V1_520K 9.dat'
-
+filepath_Gate_minus_1V = r'RawData/Gate_-0_9992V_LI_swiched_133_9V1_520K_end_133_5V1_520K 8.dat'
+filepath_Gate_1V = r'RawData/Gate_1V_LI_swiched_152_3V1_520K_end_134_0V1_520K 7.dat'
+filepath_Gate_1_5V = r'RawData/Gate_1_5V_LI_swiched_135_5V1_520K_end_134_7V1_520K 6.dat'
 # im Array: filepath, cut_rampup_anfang (Zeit), cut_rampup_ende, cut_rampdown_anfang, cut_rampdown_ende, temp, temp_fehler, U_gate, switched:boolean
-Messungen_dict = {'4.2K': {'path': filepath_4_2K, 'switched': False, 'temp': 3.99, 'temp_fehler': 0.031, 'U_gate': -0.25,'cut_rampup_anfang': 5.42, 'cut_rampup_ende':441, 'cut_rampdown_anfang':459, 'cut_rampdown_ende':896},                 
-                    '3K' : {'path': filepath_3K, 'cut_rampup_anfang': 6.01, 'cut_rampup_ende': 439, 'cut_rampdown_anfang': 447, 'cut_rampdown_ende': 882, 'temp': 2.97, 'temp_fehler': 0.0, 'U_gate': -0.25, 'switched': False},
-                    '2.1K' : {'path': filepath_2_1K, 'cut_rampup_anfang': 5.2, 'cut_rampup_ende': 440, 'cut_rampdown_anfang': 445.5, 'cut_rampdown_ende': 880, 'temp': 2.11, 'temp_fehler': 0.01, 'U_gate': -0.25, 'switched': False},
-                    '1.4K' : {'path': filepath_1_4K, 'cut_rampup_anfang': 5.4, 'cut_rampup_ende': 440, 'cut_rampdown_anfang': 455, 'cut_rampdown_ende': 889, 'temp': 1.52, 'temp_fehler': 0.0, 'U_gate': -0.25, 'switched': False},
-                    '1.4K_switched' : {'path': filepath_1_4K_switched, 'cut_rampup_anfang': 4.7, 'cut_rampup_ende': 439, 'cut_rampdown_anfang': 456, 'cut_rampdown_ende': 889, 'temp': 1.495, 'temp_fehler': 0.025, 'U_gate': -0.25, 'switched': True},
-                    'Gate_minus_1_5v' : {'path': filepath_Gate_minus_1_5V, 'cut_rampup_anfang': 5.93, 'cut_rampup_ende': 440, 'cut_rampdown_anfang': 455, 'cut_rampdown_ende': 889, 'temp': 1.495, 'temp_fehler': 0.025, 'U_gate': -1.5, 'switched': True},
+Messungen_dict = {'4.2K': {'path': filepath_4_2K, 'switched': False, 'temp': 3.99, 'temp_fehler': 0.031, 'U_gate': -0.25},                 
+                    '3K' : {'path': filepath_3K, 'temp': 2.97, 'temp_fehler': 0.0, 'U_gate': -0.25, 'switched': False},
+                    '2.1K' : {'path': filepath_2_1K, 'temp': 2.11, 'temp_fehler': 0.01, 'U_gate': -0.25, 'switched': False},
+                    '1.4K' : {'path': filepath_1_4K, 'temp': 1.52, 'temp_fehler': 0.0, 'U_gate': -0.25, 'switched': False},
+                    '1.4K_switched' : {'path': filepath_1_4K_switched, 'temp': 1.495, 'temp_fehler': 0.025, 'U_gate': -0.25, 'switched': True},
+                    'Gate_minus_1_5V' : {'path': filepath_Gate_minus_1_5V, 'temp': 1.495, 'temp_fehler': 0.025, 'U_gate': -1.5, 'switched': True},
+                    'Gate_minus_1V' : {'path': filepath_Gate_minus_1V, 'temp': 1.52, 'temp_fehler': 0.0, 'U_gate': -1.0, 'switched': True},
+                    'Gate_1V' : {'path': filepath_Gate_1V, 'temp': 1.495, 'temp_fehler': 0.025, 'U_gate': 1.0, 'switched': True},
+                    'Gate_1_5V' : {'path': filepath_Gate_1_5V, 'temp': 1.52, 'temp_fehler': 0.0, 'U_gate': 1.5, 'switched': True}
                     }
                     
                     
