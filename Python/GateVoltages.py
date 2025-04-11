039a532fe162bd9ab349c82e8d02b18bd3fc514f
Lukas <lukasxhein@gmail.com>
2025-04-11 18:26:04 +0200

    commit it in die zweite

--- Python/GateVoltages.py
+++ Python/GateVoltages.py
@@ -3,13 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import DataPlotter as dp
+from macroswriter import writeLatexMacro
 
+Gates = ['-1.5', '-1', '-0.25', '1', '1.5']
 # Datenbasis
 Data = ['Gate_minus_1_5V', 'Gate_minus_1V', '1.4K_switched', 'Gate_1V', 'Gate_1_5V']
-
+def writeN1Macros(nTable, nErrorTable, temperatures):
+    """
+    Schreibt die Makros für n1-Werte in die Datei macros.tex.
+    """
+    for i in range(len(temperatures)-1):
+        macro_name = f"n_{temperatures[i+1].replace('.', '').replace('K', '')}"  
+        writeLatexMacro(macro_name, nTable[i+1], unit=r"\text{m}^{-2}", error=nErrorTable[i],
+                        filepath="Paper/Latex/macros.tex")
+    writeLatexMacro('n-15', nTable[0]*-1,unit=r"\text{m}^{-2}", error=nErrorTable[0],
+                        filepath="Paper/Latex/macros.tex")
 # Daten extrahieren
 nTable, nErrorTable, BTable, rhoXYTable, slopeTable, rhoXXTable = getN1(Data)
 
+writeN1Macros(nTable, nErrorTable, Gates)
 # Plot für rhoXY und rhoXX
 plt.figure(figsize=(10, 6))
 plt.plot(BTable[0], rhoXYTable[0]/1000, label=r"$\rho_{XY}$", color="blue")
