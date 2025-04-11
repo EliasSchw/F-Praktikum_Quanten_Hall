4e2a81e1bd369fcf718fdfa761d5199605900343
Lukas <lukasxhein@gmail.com>
2025-04-11 14:43:08 +0200

    Füge GateVoltages.py hinzu: Datenextraktion und Plot für rhoXY und rhoXX

---
+++ Python/GateVoltages.py
@@ -0,0 +1,22 @@
+from ChargeCarrierDensity import getN1
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import DataPlotter as dp
+
+# Datenbasis
+Data = ['Gate_minus_1_5V', 'Gate_minus_1V', '1.4K_switched', 'Gate_1V', 'Gate_1_5V']
+
+# Daten extrahieren
+nTable, nErrorTable, BTable, rhoXYTable, slopeTable, rhoXXTable = getN1(Data)
+
+# Plot für rhoXY und rhoXX
+plt.figure(figsize=(10, 6))
+plt.plot(BTable[1], rhoXYTable[1]/1000, label=r"$\rho_{XY}$", color="blue")
+plt.plot(BTable[1], rhoXXTable[1]/1000, label=r"$\rho_{XX}$", color="red")
+plt.xlabel(r"$B$ (T)")
+plt.ylabel(r"$\rho$ ($k\Omega$)")
+plt.title('Gate Voltage = -1V')
+plt.legend()
+dp.fancyGraph()
+plt.show()
